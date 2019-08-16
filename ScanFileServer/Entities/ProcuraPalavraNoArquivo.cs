6de8047be70f43e029448d5136b4e21f5c74e9ae
Wallyson Silva <wallysonferreirasilva@gmail.com>
2019-08-16 19:44:41 -0300

    Inicio do projeto. Classe que procura palavra desejada nos arquivos

---
+++ ScanFileServer/Entities/ProcuraPalavraNoArquivo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ScanFileServer.Entities
+{
+    class ProcuraPalavraNoArquivo
+    {
+        public string CaminhoDoDiretorio { get; set; }
+        public string PalavraProcurada { get; set; }
+
+        public ProcuraPalavraNoArquivo()
+        {
+        }
+
+        public ProcuraPalavraNoArquivo(string caminhoDoDiretorio, string palavraProcurada)
+        {
+            CaminhoDoDiretorio = caminhoDoDiretorio;
+            PalavraProcurada = palavraProcurada;
+        }
+
+        int cont = 0;
+        string imprime;
+
+        public void PercorreArquivo(string Arquivo)
+        {
+            StreamReader sr;
+            sr = File.OpenText(Arquivo);
+
+            string[] stringSeparador = new string[] { " ", "!", "@", ",", ";", ", ", "; ", "#", "$", "%", "&", "*", "(", ")", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ":", ": ", "-", " - ", " -", "- " };
+
+
+            while (!sr.EndOfStream)
+            {
+                string palavras = sr.ReadLine();
+                string[] cortaPalavra = palavras.Split(stringSeparador, StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string obj in cortaPalavra)
+                {
+                    if (obj.StartsWith(PalavraProcurada, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        cont++;
+                    }
+                }
+            }
+            if (cont > 0)
+            {
+                 imprime = "\nA palavra foi encontrada " + cont + " veze(s).\n\n"
+                            + Arquivo;
+            }
+            else
+            {
+                 imprime = "A palavra não foi encontrada.";
+            }
+        }
+
+        public override string ToString()
+        {
+            return imprime;
+        }
+
+    }
+}         
---
+++ ScanFileServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using ScanFileServer.Entities;
+
+namespace ScanFileServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com o caminho do diretório: ");
+            string caminhoDoDiretorio = Console.ReadLine();
+
+            Console.Write("Entre com a palavra procurada: ");
+            string palavraProcurada = Console.ReadLine();
+
+            ProcuraPalavraNoArquivo x = new ProcuraPalavraNoArquivo(caminhoDoDiretorio, palavraProcurada);
+
+            try
+            {
+                var arquivos = Directory.EnumerateFiles(caminhoDoDiretorio, "*.*", SearchOption.AllDirectories);
+
+                foreach (string arquivoAtual in arquivos)
+                {
+                    x.PercorreArquivo(arquivoAtual);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine(x);
+
+        }
+    }
+}
